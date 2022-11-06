fe54b848b9c09a4cddeaacf41c29507bef4ea9d9
liron vaknin <45284793+lironv@users.noreply.github.com>
2022-11-06 21:52:02 +0200

    added connection via env

--- attendance/app/sftp.py
+++ attendance/app/sftp.py
@@ -1,17 +1,21 @@
 import pysftp
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 def download_csvs():
-  Hostname = "185.164.16.144"
-  Username = "lironv"
-  Password = "123456"
   localFilePath = "/app"
   cnopts = pysftp.CnOpts()
   cnopts.hostkeys = None
-  with pysftp.Connection(host=Hostname, username=Username, password=Password, cnopts=cnopts) as sftp:
+  with pysftp.Connection(host=os.environ.get('REMOTE_IP'), username=os.environ.get('REMOT_UNAME'), password=os.environ.get('REMOT_PASS'), cnopts=cnopts) as sftp:
     print("Connection successfully established ... ")
     # Switch to a remote directory
     sftp.get_r("/var/tmp/csv_files/", localFilePath)
     print("done")
+
    
+
    
+
    
