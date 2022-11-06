f8bbf8a385481fd24854c422e81aa069ec13a863
liron vaknin <45284793+lironv@users.noreply.github.com>
2022-11-06 21:55:59 +0200

    updated to use env

--- attendance/app/app.py
+++ attendance/app/app.py
@@ -2,6 +2,7 @@ from typing import List, Dict
 from flask import Flask, render_template, json, jsonify
 from pandas import DataFrame
 import time
+import os
 import mysql.connector
 import json
 import csv
@@ -9,12 +10,11 @@ import pandas as pd
 import sftp
 import createattendance
 import numpy as np
-
+from dotenv import load_dotenv
+load_dotenv()
 
 
 app = Flask(__name__)
-
-
 
 def read_csv():
 	csv_file = 'attendance.csv'
@@ -24,41 +24,36 @@ def read_csv():
 	empdata.fillna(0,inplace=True)
 	empdata.head()
 
-
+	
 def student_data() -> List[Dict]:
-    
-    connection = mysql.connector.connect(host='db',username='root',password='root',database='devopsroles')
+    connection =mysql.connector.connect(host=os.environ.get('DB_HOST'),username=os.environ.get('DB_UNAME'),password=os.environ.get('DB_PASSWORD'),database='devopsroles')
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM student_data')
     results = cursor.fetchall()
-    
-    
-    
     cursor.close()
     connection.close()
     return results
 
-
-
 def createdatabase():
-
-    connection = mysql.connector.connect(host='db',username='root',password='root',database='devopsroles')
+    connection = mysql.connector.connect(host=os.environ.get('DB_HOST'),username=os.environ.get('DB_UNAME'),password=os.environ.get('DB_PASSWORD'))
     cursor = connection.cursor()
-    
+    cursor.execute("CREATE DATABASE  IF NOT EXISTS  devopsroles")
+    connection.commit()
+    cursor.close()
+    connection.close()
+    connection =mysql.connector.connect(host=os.environ.get('DB_HOST'),username=os.environ.get('DB_UNAME'),password=os.environ.get('DB_PASSWORD'),database='devopsroles')
+    cursor = connection.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS student_data(names VARCHAR(255),names2 VARCHAR(255),dates1 VARCHAR(30),dates2 VARCHAR(30),dates3 VARCHAR(30),dates4 VARCHAR(30),dates5 VARCHAR(30),dates6 VARCHAR(30),dates7 VARCHAR(30),dates8 VARCHAR(30),dates9 VARCHAR(30),dates10 VARCHAR(30),dates11 VARCHAR(30),dates12 VARCHAR(30),dates13 VARCHAR(30),dates14 VARCHAR(30),sum VARCHAR(25)) DEFAULT CHARSET=utf8") 
-    
+
     for i,row in empdata.iterrows():
             #here %S means string values 
             sql = "INSERT INTO devopsroles.student_data VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s )"
             empdata.fillna(0)
             #print(sql, tuple(row))
             cursor.execute(sql, tuple(row))
-            
             connection.commit()
     cursor.close()
     connection.close()
-    
-
 @app.route('/')
 def index() -> str:
     return render_template("home.html")
