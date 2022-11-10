9b1fc7e55525c2814fb431192c928ee3e778d5e8
liron vaknin <45284793+lironv@users.noreply.github.com>
2022-11-10 13:45:52 +0200

    fix

--- attendance/app/sftp.py
+++ attendance/app/sftp.py
@@ -8,7 +8,7 @@ def download_csvs():
   localFilePath = "/app"
   cnopts = pysftp.CnOpts()
   cnopts.hostkeys = None
-  with pysftp.Connection(host=os.environ.get('REMOTE_IP'), username=os.environ.get('REMOT_UNAME'), password=os.environ.get('REMOT_PASS'), cnopts=cnopts) as sftp:
+  with pysftp.Connection(host=os.environ.get('REMOTE_IP'), username=os.environ.get('REMOT_UNAME'), password=os.environ.get('REMOT_PASS'),port=22 , cnopts=cnopts) as sftp:
     print("Connection successfully established ... ")
     # Switch to a remote directory
     sftp.get_r("/var/tmp/csv_files/", localFilePath)
